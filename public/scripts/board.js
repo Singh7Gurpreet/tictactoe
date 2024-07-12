532fe4c5ebf5fc1572a405cad8097d464b7ab04f
Gurpreet Singh <parasrockz81@gmail.com>
2024-07-12 00:19:47 -0600

    Did some board implementation

---
+++ public/scripts/board.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.elementButton');
+
+async function postReqeuest(tileSelected) {
+  // send tile selected through socket to server
+}
+
+buttons.map((button) => {
+  button.addEventListener('click', () => {});
+});
+
+//will receive socket from server to apply changes
+//made by opponent on board
